7d23412aad508b7e7934583fde3e08e477809657
Esteban Cáceres <estebancacerest@gmail.com>
2024-02-13 21:04:15 -0300

    desafio guiado opcional centro medico

---
+++ assets/js/main.js
@@ -0,0 +1,222 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~array radiologia
+const radiologia = [
+  {
+    hora: "11:00",
+    especialista: "IGNACIO SCHULZ",
+    paciente: "FRANCISCA ROJAS",
+    rut: "9878782-1",
+    prevision: "FONASA",
+  },
+  {
+    hora: "11:30",
+    especialista: "FEDERICO SUBERCASEAUX",
+    paciente: "PAMELA ESTRADA",
+    rut: "15345241-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:00",
+    especialista: "FERNANDO WURTHZ",
+    paciente: "ARMANDO LUNA",
+    rut: "16445345-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:30",
+    especialista: "ANA MARIA GODOY",
+    paciente: "MANUEL GODOY",
+    rut: "17666419-0",
+    prevision: "FONASA",
+  },
+  {
+    hora: "16:00",
+    especialista: "PATRICIA SUAZO",
+    paciente: "RAMON ULLOA",
+    rut: "14989389-K",
+    prevision: "FONASA",
+  },
+];
+
+// iterar el array de objetos con un CICLO FOR //radiologia itera con i. hora.
+let tabla = "";
+for (let i = 0; i < radiologia.length; i++) {
+  tabla += `
+    <tr>
+    <td>${radiologia[i].hora}</td>
+    <td>${radiologia[i].especialista}</td>
+    <td>${radiologia[i].paciente}</td>
+    <td>${radiologia[i].rut}</td>
+    <td>${radiologia[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaRadiologia").innerHTML = tabla;
+
+/* 
+// ITERAR CON FOR IN
+for (let i in radiologia) {
+  console.log(radiologia[i].especialista);
+}
+
+//ITERAR CON FOR...OF
+for (let i of radiologia) {
+  console.log(i.rut)
+}  */
+
+console.log(radiologia.length);
+//5 elementos. primero Indice obj 0 y ultimo obj 4.
+
+//mostrar nombre y prevision de primera y ultima consulta,
+document.getElementById("pacientesRadiologia").innerHTML = `
+  <h5>Radiologia || Primera y última atención del día</h5>
+  <b>Primera Atencion:</b>  ${radiologia[0].paciente} - ${
+  radiologia[0].prevision
+} | <b>Última Atencion:</b>  ${radiologia[radiologia.length - 1].paciente} - ${
+  radiologia[radiologia.length - 1].prevision
+}
+`;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~arrays traumatologia
+const traumatologia = [
+  {
+    hora: "8:00",
+    especialista: "MARIA PAZ ALTUZARRA",
+    paciente: "PAULA SANCHEZ",
+    rut: "15554774-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL ARAYA",
+    paciente: "ANGÉLICA NAVAS",
+    rut: "15444147-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "MARIA ARRIAGADA",
+    paciente: "ANA KLAPP",
+    rut: "17879423-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "ALEJANDRO BADILLA",
+    paciente: "FELIPE MARDONES",
+    rut: "1547423-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "CECILIA BUDNIK",
+    paciente: "DIEGO MARRE",
+    rut: "16554741-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "12:00",
+    especialista: "ARTURO CAVAGNARO",
+    paciente: "CECILIA MENDEZ",
+    rut: "9747535-8",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:30",
+    especialista: "ANDRES KANACRI",
+    paciente: "MARCIAL SUAZO",
+    rut: "11254785-5",
+    prevision: "ISAPRE",
+  },
+];
+
+// ITERAR CON FOR IN
+let tabla2 = "";
+for (let i in traumatologia) {
+  tabla2 += `
+    <tr>
+    <td>${traumatologia[i].hora}</td>
+    <td>${traumatologia[i].especialista}</td>
+    <td>${traumatologia[i].paciente}</td>
+    <td>${traumatologia[i].rut}</td>
+    <td>${traumatologia[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaTraumatologia").innerHTML = tabla2;
+
+document.getElementById(
+  "pacientesTraumatologia"
+).innerHTML = `<h5>Traumatologia || Primera y última atención del día</h5><b>Primera Atencion:</b>  ${
+  traumatologia[0].paciente
+} - ${traumatologia[0].prevision} | <b>Última Atencion:</b>  ${
+  traumatologia[traumatologia.length - 1].paciente
+} - ${traumatologia[traumatologia.length - 1].prevision}</p>`;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~array traumatologia
+
+const dental = [
+  {
+    hora: "8:30",
+    especialista: "ANDREA ZUÑIGA",
+    paciente: "MARCELA RETAMAL",
+    rut: "11123425-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "MARIA PIA ZAÑARTU",
+    paciente: "ANGEL MUÑOZ",
+    rut: "9878789-2",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "SCARLETT WITTING",
+    paciente: "MARIO KAST",
+    rut: "7998789-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:00",
+    especialista: "FRANCISCO VON TEUBER",
+    paciente: "KARIN FERNANDEZ",
+    rut: "18887662-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:30",
+    especialista: "EDUARDO VIÑUELA",
+    paciente: "HUGO SANCHEZ",
+    rut: "17665461-4",
+    prevision: "FONASA",
+  },
+  {
+    hora: "14:00",
+    especialista: "RAQUEL VILLASECA",
+    paciente: "ANA SEPULVEDA",
+    rut: "14441281-0",
+    prevision: "ISAPRE",
+  },
+];
+
+let tabla3 = "";
+for (let i in dental) {
+  tabla3 += `
+    <tr>
+    <td>${dental[i].hora}</td>
+    <td>${dental[i].especialista}</td>
+    <td>${dental[i].paciente}</td>
+    <td>${dental[i].rut}</td>
+    <td>${dental[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaDental").innerHTML = tabla3;
+
+document.getElementById(
+  "pacientesDental"
+).innerHTML = `<h5>Traumatologia || Primera y última atención del día</h5><b>Primera Atencion:</b>  ${
+  dental[0].paciente
+} - ${dental[0].prevision} | <b>Última Atencion:</b>  ${
+  dental[dental.length - 1].paciente
+} - ${dental[dental.length - 1].prevision}</p>`;
